28e0acd172056977acc01c4943bc39f341184ee7
oratiek <oratiek522@gmail.com>
2024-02-17 18:03:32 +0900

    added get_floor method to Node.

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import os
 import sys
 
+TAB_SIZE = 4
+
 class Node:
     def __init__(self, text):
+        self.floor = self.get_floor(text)
         self.content = ""
         self.parent = ""
         self.children = []
-
+    
+    def get_floor(self, text):
+        # タブの数をみる
+        floor = 0
+        for char in text:
+            if char != " ":
+                break
+            floor += 1
+        return floor // TAB_SIZE
+            
 def parse(path):
     nodes = []
     with open(path, "r") as f:
         for row in f.readlines():
-            print(row)
+            nodes.append(Node(row))
+
 
 if __name__ == "__main__":
     path = "test.md"
